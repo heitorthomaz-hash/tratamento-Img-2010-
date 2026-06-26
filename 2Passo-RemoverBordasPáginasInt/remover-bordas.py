a6c05a97082668bf7074b94e37cb224e5ea441ec
nheittor <heitor.thomaz@escola.pr.gov.br>
2026-06-26 08:28:24 -0300

    Retirei as bordas externas

--- 2Passo-RemoverBordasPáginasInt/remover-bordas.py
+++ 2Passo-RemoverBordasPáginasInt/remover-bordas.py
@@ -25,7 +25,7 @@ for nome_arquivo in os.listdir(pasta_imagens):
 
         largura, altura = imagem.size
 
-        caixa_corte = (38, 114, largura -49, altura - 71) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
+        caixa_corte = (116, 344, largura -151, altura - 216) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
         imagem_cortada = imagem.crop(caixa_corte)
 
         caminho_saida = os.path.join(pasta_saida, nome_arquivo)
