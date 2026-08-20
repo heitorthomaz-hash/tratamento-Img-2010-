3e49238e7d3592acea485b15ce2b9656fc1d74d0
heitor <heitor.thomaz@escola.pr.gov.br>
2026-08-20 11:19:33 -0300

    Terminei passo 9

--- 9Passo-TratarNomes/renomear-outras.py
+++ 9Passo-TratarNomes/renomear-outras.py
@@ -19,7 +19,7 @@ OBS3: para cada vez que executar esse código, faça:
 import os
 
 def renomear_questoes_simples():
-    pasta = "54-63" # ATUALIZAR com o nome da pasta das questões que você vai arrumar 
+    pasta = "85-90" # ATUALIZAR com o nome da pasta das questões que você vai arrumar 
     
     if not os.path.exists(pasta):
         print(f"Pasta {pasta} não encontrada!")
@@ -29,11 +29,11 @@ def renomear_questoes_simples():
     mapeamento = {}
         
     # Exemplo: parte_00x a parte_00y -> questao-x a questao-y
-    for i in range(64, 68+1):    # atualize seu for com o número da primeira imagem "parte_AlgumaCoisa.png" até o número da última imagem "parte_AlgumaCoisa.png" mais 1 da pasta
+    for i in range(100, 105+1):    # atualize seu for com o número da primeira imagem "parte_AlgumaCoisa.png" até o número da última imagem "parte_AlgumaCoisa.png" mais 1 da pasta
         antigo = f"parte_{i:03d}.png"
         #novo = f"questao-{i+78}-espanhol.png"  # faça uma conta: se a primeira pagina for 
         #novo = f"questao-{i+78}-ingles.png"
-        novo = f"questao-{i-10}.png" # faça uma conta: se o i do teu for está em 2, e precisa virar questão 35, como você transforma 2 em 35? faça a conta e coloque dentro da concatenação
+        novo = f"questao-{i-15}.png" # faça uma conta: se o i do teu for está em 2, e precisa virar questão 35, como você transforma 2 em 35? faça a conta e coloque dentro da concatenação
         
         mapeamento[antigo] = novo
     
